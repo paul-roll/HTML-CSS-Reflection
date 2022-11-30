ffe301462fccadf5ae4d4b650c26a7acd914168f
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2022-11-30 14:43:30 +0000

    Added sticky header

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,27 @@
-// const header = document.getElementsByTagName("header")[0];
+
+function headerSlideIn() {
+    header.classList.add("sticky");
+    $("header").stop().css("top", "-100%").animate({"top": "0"}, 300);
+}
+
+function headerSlideOut() {
+    $("header").stop().css("top", "0").animate({"top": "-100%"}, 300, function() {
+        header.classList.remove("sticky");
+    });
+    
+}
+
+const header = document.getElementsByTagName("header")[0];
+
+
+let i = $(this).scrollTop();
+window.onscroll = function() {
+    if ((!header.classList.contains("sticky")) && ($(this).scrollTop()) < i) {
+        headerSlideIn();
+        
+        
+    } else if (($("header").css("top") === "0px") && ($(this).scrollTop()) > i) {
+        headerSlideOut();
+    }
+    i = $(this).scrollTop();
+};
