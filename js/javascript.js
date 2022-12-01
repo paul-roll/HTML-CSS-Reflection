35e80622505adc291ad0794660ec1fcc6524c53d
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2022-12-01 16:07:34 +0000

    Added a lot of functionality to the sidebar. - Size the width of sidebar based on screen size. - Fixed page margins to move the correct amount - Fixed page margins to adapt if the window is resized. - While sidebar; prevent clicks that are not on the burger or sidebar. - While header is sticky, hover prevents page scrolling. - While sidebar open, prevents page from scrolling - Sidebar can scroll. - - Changed the scroll blocking method to not conflict with each other. - Removed sticky from header when scrolled to top to allow scrolling. - Prevent sticky header from sliding in before it has been scrolled past

--- js/javascript.js
+++ js/javascript.js
@@ -16,11 +16,11 @@ const header = document.getElementsByTagName("header")[0];
 
 let i = $(this).scrollTop();
 window.onscroll = function() {
-    if ((!header.classList.contains("sticky")) && ($(this).scrollTop()) < i) {
+    if ((header.classList.contains("sticky")) && ($(this).scrollTop() === 0)) {
+        header.classList.remove("sticky");
+    } else if ( (!header.classList.contains("sticky")) && ($(this).scrollTop() < i) && ($(this).scrollTop() > header.offsetHeight) ) {
         headerSlideIn();
-        
-        
-    } else if (($("header").css("top") === "0px") && ($(this).scrollTop()) > i) {
+    } else if ( ($("header").css("top") === "0px") && ($(this).scrollTop() > i) ) {
         headerSlideOut();
     }
     i = $(this).scrollTop();
@@ -37,14 +37,75 @@ $(document).ready(function(){
 });
 
 
-$(".hamburger").on("click", function() {
-    $(".hamburger").toggleClass("is-active");
-    if ($(".hamburger").hasClass("is-active")) {
-        $("body").css("margin-left", "-=200");
-        $(".sidebar").removeClass("hidden");
-    } else {
-        $("body").css("margin-left", "+=200");
+function setMargins(value, hide) {
+    if (hide) {
+        $("body").css("margin-left", `+=${value}`);
+        $("body").css("margin-right", `-=${value}`);
         $(".sidebar").addClass("hidden");
+    } else {
+        $("body").css("margin-left", `-=${value}`);
+        $("body").css("margin-right", `+=${value}`);
+        $(".sidebar").removeClass("hidden");
+    }
+}
+
+
+const breakpointMD = 992;
+
+let sidebarWide = false;
+if ($(window).outerWidth() >= breakpointMD) {
+    sidebarWide = true;
+}
+
+function sidebarWidth() {
+    if ($(window).outerWidth() < breakpointMD) {
+        sidebarWide = false;
+        return 275;
+    } else {
+        sidebarWide = true;
+        return 350;
+    }
+}
+
+$(window).on("resize", function() {
+    if ($(".hamburger").hasClass("is-active")) {
+        if ((sidebarWide) && ($(window).outerWidth() < breakpointMD)) {
+            setMargins(-75, false);
+            sidebarWide = false;
+        } else if ((!sidebarWide) && ($(window).outerWidth() >= breakpointMD)) {
+            setMargins(75, false);
+            sidebarWide = true;
+        }
     }
 });
 
+
+$("body").on("click", function(e) {
+
+    // clicks on hamburger
+    if ($(e.target).hasClass("hamburger")) {
+        $(".hamburger").toggleClass("is-active");
+        if ($(".hamburger").hasClass("is-active")) {
+            setMargins(sidebarWidth(), false);
+            $("body").addClass("sidebar-scroll-lock");
+        } else {
+            setMargins(sidebarWidth(), true);
+            $("body").removeClass("sidebar-scroll-lock");
+        }
+
+    // clicks not on sidebar
+    } else if (($(".hamburger").hasClass("is-active")) && (!$(e.target).parents(".sidebar").length)) {
+        e.preventDefault();
+    }
+});
+
+$("header").on("mouseenter", function() {
+    if ( (!$(".hamburger").hasClass("is-active")) && ($("header").hasClass("sticky")) ) {
+        $("body").addClass("hover-scroll-lock");
+    }
+});
+$("header").on("mouseleave", function() {
+    if ( (!$(".hamburger").hasClass("is-active")) && ($("header").hasClass("sticky")) ) {
+        $("body").removeClass("hover-scroll-lock");
+    }
+});
