7c9f1b3649ff75ceee4b31280dcb7945642938bb
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-01-10 10:45:30 +0000

    Moved the email section into its own include file Renamed all instances of "email" to "newsletter" in the html and sass

--- contact.php
+++ contact.php
@@ -2,31 +2,7 @@
 
             <main>
 
-                <!-- Email -->
-                <section class="email">
-                    <div class="wrapper">
-                        <h2>Email Newsletter Sign-Up</h2>
-                        <form>
-                            <div class="flex-container">
-                                <div>
-                                    <label class="required" for="name">Your Name</label><br>
-                                    <input id="name" type="text">
-                                </div>
-                                <div>
-                                    <label class="required" for="email">Your Email</label><br>
-                                    <input id="email" type="text">
-                                </div>
-                            </div>
-                            <div class="checkboxwrapper">
-                                <input id="checkbox" type="checkbox">
-                                <label class="fa-solid" for="checkbox"></label>
-                                <label class="checkbox-label" for="checkbox">Please tick this box if you wish to receive marketing information from us. Please see our <a href="#">Privacy Policy</a> for more information on how we keep your data safe.</label>
-                            </div>
-                            <br>
-                            <input id="subscribe" type="submit" value="Subscribe">
-                        </form>
-                    </div>
-                </section>
+                <?php include("inc/newsletter.php"); ?>
 
             </main>
 
