d0d82da9b88508bb30dad663712da21342889df3
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-01-11 08:24:26 +0000

    styling contact.php office section

--- contact.php
+++ contact.php
@@ -20,20 +20,23 @@
                         <h2>Our Offices</h2>
                         <div class="flex-container">
 <?php foreach ($offices as $office) {
-echo"                        <div class='news-item bespoke-software'>\n";
+echo"<div>\n";    
+echo"                        <div class='office-item'>\n";
 echo"                            <a href='#'>\n";
 echo"                                <img class='news-image' src='img/offices/" . $office["office"] . ".jpg' alt=''>\n";
 echo"                            </a>\n";
-echo"                            <div class='wrapper90pc'>\n";
-echo"                                <p>" . $office["office"] . "</p>\n";
+echo"                            <div>\n";
+echo"                                <h3>" . $office["office"] . "</h3>\n";
 echo"                                <ul>\n";
     echoList($office["address"]);
 echo"                                </ul>\n";
 echo"                                <p>" . $office["phone"] . "</p>\n";
 echo"                                <div class='btn'>View More</div>\n";
 echo"                           </div>\n";
-echo"<iframe width='100%' height='315px' id='mapcanvas' src='https://maps.google.com/maps?q=Netmatters," . $office["address"][4] . "&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=&amp;output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:100%;'></div></div></iframe>";
+
 echo"                        </div>\n";
+echo"<iframe width='100%' height='300px' id='mapcanvas' src='https://maps.google.com/maps?q=Netmatters," . $office["address"][4] . "&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=&amp;output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'><div style='overflow:hidden;'><div id='gmap_canvas' style='height:100%;width:100%;'></div></div></iframe>";
+echo"</div>\n";   
 } ?>
                         </div>
                     </div>
