04ed40ac2e5f61a9b016335678097281ee0fc02a
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-01-11 10:50:17 +0000

    Contact form structure and initial styling

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@
 echo"<div>\n";    
 echo"                        <div class='office-item'>\n";
 echo"                            <a href='#'>\n";
-echo"                                <img class='news-image' src='img/offices/" . $office["office"] . ".jpg' alt=''>\n";
+echo"                                <img class='office-image' src='img/offices/" . $office["office"] . ".jpg' alt=''>\n";
 echo"                            </a>\n";
 echo"                            <div>\n";
 echo"                                <h3>" . $office["office"] . "</h3>\n";
@@ -41,6 +41,63 @@ echo"</div>\n";
                         </div>
                     </div>
                 </section>
+
+                <!-- Contact -->
+                <section class="contact">
+                    <div class="wrapper">
+                        <div class="flex-container">
+                            <div class="flex-item">X</div>
+                            <div class="flex-item">
+                                <form>
+                                    <div class="flex-form">
+                                        <div class="half">
+                                            <label class="required" for="name">Your Name</label><br>
+                                            <input id="name" type="text">
+                                        </div>
+                                        <div class="half">
+                                            <label class="required" for="company">Company Name</label><br>
+                                            <input id="company" type="text">
+                                        </div>
+                                        <div class="half">
+                                            <label class="required" for="email">Your Email</label><br>
+                                            <input id="email" type="text">
+                                        </div>
+                                        <div class="half">
+                                            <label class="required" for="phone">Your Telephone Number</label><br>
+                                            <input id="phone" type="text">
+                                        </div>
+                                        <div>
+                                            <label class="required" for="subject">Subject</label><br>
+                                            <input id="subject" type="text">
+                                        </div>
+                                        <div>
+                                            <label class="required" for="message">Message</label><br>
+                                            <input id="message" type="text">
+                                        </div>
+
+                                        <div class="checkboxwrapper">
+                                            <input id="checkbox" type="checkbox">
+                                            <label class="fa-solid" for="checkbox"></label>
+                                            <label class="checkbox-label" for="checkbox">Please tick this box if you wish to receive marketing information from us. Please see our <a href="#">Privacy Policy</a> for more information on how we keep your data safe.</label>
+                                        </div>
+
+                                        <div>
+                                            <input class="left" id="enquiry" type="submit" value="Send Enquiry">
+                                            <div class="right">* Fields Required</div>
+                                        </div>
+
+                                    </div>
+                                    
+                                </form>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </section>
+
 
                 <?php include("inc/newsletter.php"); ?>
 
